9049cc3a69fdd45044f54e5891101b7b1d498a78
Winston <winston@mac.local>
2018-07-07 14:28:18 -0500

    updating base inputs

---
+++ Python/Base_Inputs.py
@@ -0,0 +1,23 @@
+#-------------------------------------
+# Base Inputs
+#-------------------------------------
+
+#Imports-----------------------------
+import warnings
+import pandas
+import os
+import sys
+import numpy as np
+import pandas as pd
+#------------------------------------
+
+#Functions--------------------------
+
+#-----------------------------------
+
+
+#-------------------------------------
+# End of Base Inputs
+#-------------------------------------
+message = 'Base Inputs Loaded'
+print message
